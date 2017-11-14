1bd3198ffb5e4170c4abd1818ee971d5e97e8f52
Uri Shaked <uri@salsa4fun.co.il>
2017-11-14 09:40:04 +0100

    feat: add aframe + plugins + aframe pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import 'aframe-fireball-component';
+import 'aframe-starnest-component';
+import 'aframe-animation-component';
+import { AframePipe } from './aframe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AframePipe
   ],
   imports: [
     BrowserModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
